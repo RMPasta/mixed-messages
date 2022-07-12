3ed22bd43eddc9d59638ac539cd6ea307a8a04d1
Ryan Malmos <rmpasta.code@gmail.com>
2022-07-12 15:11:04 -0400

    Add more styling on button like cursor and hover/active states

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const feeling = ['relaxed', 'chill', 'stressed', 'overwhelmed', 'ecstatic', 'num
 
 const modifier = ['a lot of ', 'less ', 'more ', 'massive amounts of ', 'an extreme lack of '];
 
-const thing = ['money', 'love', 'frinedship', 'passion', 'respect', 'surprises', 'injuries', 'health', 'prosperity', 'resolve'];
+const thing = ['money', 'love', 'friendship', 'passion', 'respect', 'surprises', 'injuries', 'health', 'prosperity', 'resolve', 'energy'];
 
-const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job opportunity', 'an enemy', 'imminent danger', 'catacalismic disaster', 'a sense of invincibility', 'a supernatural event', 'a paranormal happening', 'your wildest desires'];
+const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job opportunity', 'an enemy', 'imminent danger', 'catacalismic disaster', 'a sense of invincibility', 'a supernatural event', 'a paranormal happening', 'your wildest desires', 'have a crazy party'];
 //random options above
 //function logs random index of options array... one for each array
 
@@ -37,7 +37,7 @@ function randHappens() {
 }
 
 function randMessage() {
-writeMessage = document.write('<p style="color: darkblue; text-align: center;">Hello Human, today you will feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even run into ' + randHappens() + '. Come back tomorrow morning for another prediction about your day.</p>')
+writeMessage = document.write('<p style="color: darkblue; text-align: center; font-family: cursive; font-size: 1.4em;">Hello Human, today you will feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even run into ' + randHappens() + '. Come back tomorrow morning for another prediction about your day.</p>')
 return writeMessage;
 }
 
