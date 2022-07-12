f9639f2d690c238df84bcae5196a6a6623b60998
Ryan Malmos <rmpasta.code@gmail.com>
2022-07-12 11:16:46 -0400

    Add arrays and randomizer functions

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-function pageLoad() {
-    console.log('hello, lets get started');
+
+const feeling = ['relaxed', 'chill', 'stressed', 'overwhelmed', 'ecstatic', 'numb', 'joyous', 'comfortable', 'anxious', 'impatient', 'like everyone is out to get you', 'existential dread'];
+
+const modifier = ['a lot of ', 'less ', 'more ', 'massive amounts of ', 'an extreme lack of '];
+
+const thing = ['money', 'love', 'frinedship', 'passion', 'respect', 'surprises', 'injuries', 'health', 'prosperity', 'resolve'];
+
+const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job opportunity', 'an enemy', 'imminent danger', 'catacalismic disaster', 'a sense of invincibility', 'a supernatural event', 'a paranormal happening', 'your wildest desires'];
+//random options above
+//function logs random index of options array... one for each array
+
+//random feeling
+function randFeeling() {
+    let randIndex = Math.floor(Math.random() * feeling.length);
+    return feeling[randIndex];
 }
 
-pageLoad();
+
+//random modifier
+function randModifier() {
+    let randIndex = Math.floor(Math.random() * modifier.length);
+    return modifier[randIndex];
+}
+
+
+//random thing
+function randThing() {
+    let randIndex = Math.floor(Math.random() * thing.length);
+    return thing[randIndex];
+}
+
+
+//random event
+function randHappens() {
+    let randIndex = Math.floor(Math.random() * happens.length);
+    return happens[randIndex];
+}
+
+
+console.log('Hello Human, today you will feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even run into ' + randHappens() + '. Come back tomorrow morning for another prediction about your day. :)')
