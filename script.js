d5f1b3beb02b4eb21e55cba7947cc9f7d6a36365
Ryan Malmos <rmpasta.code@gmail.com>
2022-07-12 15:19:29 -0400

    Adjust text a bit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const modifier = ['a lot of ', 'less ', 'more ', 'massive amounts of ', 'an extr
 
 const thing = ['money', 'love', 'friendship', 'passion', 'respect', 'surprises', 'injuries', 'health', 'prosperity', 'resolve', 'energy', 'water', 'food'];
 
-const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job opportunity', 'an enemy', 'imminent danger', 'catacalismic disaster', 'a sense of invincibility', 'a supernatural event', 'a paranormal happening', 'your wildest desires', 'a crazy party'];
+const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job opportunity', 'an enemy', 'imminent danger', 'catacalismic disaster', 'a sense of invincibility', 'a supernatural event', 'a paranormal event', 'your wildest desires', 'a crazy party'];
 //random options above
 //function logs random index of options array... one for each array
 
@@ -37,7 +37,7 @@ function randHappens() {
 }
 
 function randMessage() {
-writeMessage = document.write('<p style="color: darkblue; text-align: center; font-family: cursive; font-size: 1.4em;">Hello Human, today you will feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even happen upon ' + randHappens() + '. Come back tomorrow morning for another prediction about your day.</p>')
+writeMessage = document.write('<p style="padding: 2em 2em; color: darkblue; text-align: center; font-family: cursive; font-size: 1.4em; margin: 0 auto;">Hello Human, today you may feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even happen upon ' + randHappens() + '. Come back tomorrow morning for another prediction about your day.</p>')
 return writeMessage;
 }
 
