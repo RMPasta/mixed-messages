7cc5610ac71bf9a89842331c7cea74e4bb6fc7ce
Ryan Malmos <rmpasta.code@gmail.com>
2022-07-14 09:34:09 -0400

    Change randomizer function and message

--- script.js
+++ script.js
@@ -9,35 +9,13 @@ const happens = ['a giant bird', 'an old friend', 'your next lover', 'a job oppo
 //random options above
 //function logs random index of options array... one for each array
 
-//random feeling
-function randFeeling() {
-    let randIndex = Math.floor(Math.random() * feeling.length);
-    return feeling[randIndex];
-}
-
-
-//random modifier
-function randModifier() {
-    let randIndex = Math.floor(Math.random() * modifier.length);
-    return modifier[randIndex];
-}
-
-
-//random thing
-function randThing() {
-    let randIndex = Math.floor(Math.random() * thing.length);
-    return thing[randIndex];
-}
-
-
-//random event
-function randHappens() {
-    let randIndex = Math.floor(Math.random() * happens.length);
-    return happens[randIndex];
+function rWord(words) {
+    let randIndex = Math.floor(Math.random() * words.length);
+    return words[randIndex];
 }
 
 function randMessage() {
-writeMessage = document.write('<p style="padding: 2em 2em; color: darkblue; text-align: center; font-family: cursive; font-size: 1.4em; margin: 0 auto;">Hello Human, today you may feel ' + randFeeling() + '. You should expect ' + randModifier() + randThing() + ' and you might even happen upon ' + randHappens() + '. Come back tomorrow morning for another prediction about your day.</p>')
+writeMessage = document.write('<p style="padding: 2em 2em; color: darkblue; text-align: center; font-family: cursive; font-size: 1.4em; margin: 0 auto;">Hello Human, today you may feel ' + rWord(feeling) + '. You should expect ' + rWord(modifier) + rWord(thing) + ' and you might even happen upon ' + rWord(happens) + '. Come back tomorrow morning for another prediction about your day.</p>')
 return writeMessage;
 }
 
